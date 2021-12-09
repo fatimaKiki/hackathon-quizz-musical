c17cb949fd6d5bac49915ae18cb12dfdf2538460
fatimaKiki <f.elharraoui@gmail.com>
2021-12-09 08:59:18 +0100

    inscription brouillon

--- views/page/formulaire_inscription.php
+++ views/page/formulaire_inscription.php
@@ -1,12 +1,15 @@
-<?php 
-    var_dump("yolo");
-?>
-
-<h2>Formulaire d'inscription</h2>
-
-<form action="#" method="post">
+<div class="container-fluid bg">
+    <div class="row">
+        <div class="col ">
+        <form action="#" method="post">
 <input type="text" name="nom">
 <label for="nom">Player1</label>
 
 <input type="submit" value="Inscription">
 </form>
+    </div>
+    </div>
+</div>
+
+<h2>Formulaire d'inscription</h2>
+
