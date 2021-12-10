be16b71d8b888343f9c445c6cf4bbbc89efc3a45
Manon Lespes <manonlespes@hotmail.fr>
2021-12-10 13:25:34 +0100

    problème résolu

--- controller/resultatController.php
+++ controller/resultatController.php
@@ -1,15 +1,34 @@
 <?php
 
-// include("models/User.php");
-// $player = new User();
-// $msgResultatScore = "";
+include("models/User.php");
+$player = new User();
+$msgResultatScore = "";
+
+
+if (isset($_SESSION["id"])) {
+$player1 = $player->getAllInfo($_SESSION["id"]); 
+var_dump($player1['id']);
+var_dump($player1['nom']);
+var_dump($player1['score']);
+
+//$_SESSION["id"] = $id;
+//$_SESSION["nom"] = $nom;
+
+var_dump($_SESSION["nom"]);
+
+$msgResultatScore = "<p> le score est ". $player1['score'] ."</p>";
+//$msgResultatScore = "<p> le score est ". $nom ."</p>";
+
+//     header("Location:?section=resultat");
+//     // include("views/page/resultat.php");
+}
 
 
 // /* $score = $player->addScore(9, 5);
 // var_dump("le resultat est bien inscrit dans la db");
 //  */
 
-var_dump($_SESSION['score']);
+/* var_dump($_SESSION['score']); */
 
 // if (isset($_POST["playerId"]) && ($_POST["score"])) {
 
