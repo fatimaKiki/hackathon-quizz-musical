82ad6839219a72e4a23fd20d073c7e3c8bd859ad
leilamyar <leilamyar.dev@gmail.com>
2021-12-10 11:42:55 +0100

    add(playlist) + refactor(makeUniqueIds)

--- public/js/gameUtils.js
+++ public/js/gameUtils.js
@@ -128,28 +128,6 @@ const prepareLyricsForGame = (tokenizedLyrics) => {
     lyricsText,
   }
 };
-  
-// Function make Random Unique Ids
-// Range = range of random numbers possibilities
-// OutputLength = how many unique ids we want
-// Return : an array of the same length of outputLength, containing the unique ids
-function makeRandomUniqueIds(range, outputLength) {
-
-  let arr = []
-  for (let i = 1; i <= range; i++) {
-    arr.push(i)
-  }
-
-  let randomIds = [];
-
-  for (let i = 1; i <= outputLength; i++) {
-    const random = Math.floor(Math.random() * (range - i));
-    randomIds.push(arr[random]);
-    arr[random] = arr[range - i];
-  }
-
-  return randomIds;
-}
 
 // Function makeAnswerChoices
 // 1st param : the correct lyrics to find (the correct answer choice)
--- public/js/songsData.js
+++ public/js/songsData.js
@@ -1,23 +1,107 @@
-songsDataForApi = [{
-    artist: 'Adele',
-    title: 'Easy On Me'
+songsDataForApi = [
+  {
+    artist: 'Idina Menzel',
+    title: 'Let It Go'
   },
   {
-    artist: 'Ed Sheeran',
-    title: 'Shivers'
+    artist: 'Backstreet Boys',
+    title: 'All I Have to Give'
   },
   {
-    artist: 'Dua Lipa',
-    title: 'Love Again'
+    artist: 'Aqua',
+    title: 'Barbie Girl'
   },
   {
-    artist: 'Dua Lipa',
-    title: 'Love Again'
+    artist: 'Images',
+    title: 'Les démons de minuit'
   },
+  {
+    artist: 'Jacques Brel',
+    title: 'Ne me quitte pas'
+  },
+  {
+    artist: 'Charles Aznavour',
+    title: 'La bohème'
+  },
+  {
+    artist: 'Édith Piaf',
+    title: 'La foule'
+  },
+  {
+    artist: 'Marc Lavoine',
+    title: 'Elle a les yeux revolver'
+  },
+  {
+    artist: 'Céline Dion',
+    title: 'Sous le vent'
+  },
+  {
+    artist: 'Céline Dion',
+    title: 'Pour Que Tu M\'Aimes Encore'
+  },
+  {
+    artist: 'Patrick Bruel',
+    title: 'Place des grands hommes'
+  },
+  {
+    artist: 'Début de Soirée',
+    title: 'Nuit de folie'
+  },
+  {
+    artist: 'Lio',
+    title: 'Banana Split'
+  },
+  {
+    artist: 'Idina Menzel',
+    title: 'Let It Go'
+  },
+  {
+    artist: 'Francis Cabrel',
+    title: 'Je l\'aime à mourir'
+  },
+  {
+    artist: ' Jean-Jacques Goldman',
+    title: 'Quand la musique est bonne'
+  },
+  {
+    artist: 'Alain Bashung',
+    title: 'Gaby Oh Gaby'
+  },
+  {
+    artist: 'Jean-Jacques Goldman',
+    title: 'Comme toi'
+  },
+  {
+    artist: 'Renaud',
+    title: 'Morgane de toi'
+  },
+
 ];
 
 const URL_API = 'https://api.lyrics.ovh/v1/';
 const makeUrlForApi = ({ artist, title }) => (`${URL_API}/${artist}/${title}`);
+
+// Function make Random Unique Ids
+// Range = range of random numbers possibilities
+// OutputLength = how many unique ids we want
+// Return : an array of the same length of outputLength, containing the unique ids
+function makeRandomUniqueIds(range, outputLength) {
+
+  let arr = []
+  for (let i = 1; i <= range; i++) {
+    arr.push(i)
+  }
+
+  let randomIds = [];
+
+  for (let i = 1; i <= outputLength; i++) {
+    const random = Math.floor(Math.random() * (range - i));
+    randomIds.push(arr[random]);
+    arr[random] = arr[range - i];
+  }
+
+  return randomIds;
+}
 
 const urls = [
   makeUrlForApi(songsDataForApi[0]),
