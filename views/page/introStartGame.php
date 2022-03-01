aaef7669dbfb2e15e4219c0ee27087f84234bf0b
fatimaKiki <f.elharraoui@gmail.com>
2022-03-01 14:23:46 +0100

    animation introStartGame

--- views/page/introStartGame.php
+++ views/page/introStartGame.php
@@ -1,68 +1,10 @@
 <div class="container-fluid bg">
-	<div id="animation_container" style="background-color:rgba(255, 255, 255, 1.00); width:1024px; height:768px">
-		<canvas id="canvas" width="1024" height="768" style="position: absolute; display: block; background-color:rgba(255, 255, 255, 1.00);"></canvas>
-		<div id="dom_overlay_container" style="pointer-events:none; overflow:hidden; width:1024px; height:768px; position: absolute; left: 0px; top: 0px; display: block;">
+	<div class="row ">
+		<div>
+			<img class="gif justify-content-center" src="public/images/animStart2.gif" alt="">
 		</div>
 	</div>
-	<!-- <div class="btnStart"> -->
-		<!-- <a href="?section=startGame"><img class="btnStartImg" src="public/images/btnStart.png" alt=""></a> -->
-	<!-- </div> -->
+	<div class="btnStart">
+		<a href="?section=startGame"><img class="btnStartImg" src="public/images/btnStart.png" alt=""></a>
+	</div>
 </div>
-
-
-
-<script>
-	var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
-
-	function init() {
-		canvas = document.getElementById("canvas");
-		anim_container = document.getElementById("animation_container");
-		dom_overlay_container = document.getElementById("dom_overlay_container");
-		var comp = AdobeAn.getComposition("64F4433D4F484E87A926414D200B4B5D");
-		var lib = comp.getLibrary();
-		var loader = new createjs.LoadQueue(false);
-		loader.addEventListener("fileload", function(evt) {
-			handleFileLoad(evt, comp)
-		});
-		loader.addEventListener("complete", function(evt) {
-			handleComplete(evt, comp)
-		});
-		var lib = comp.getLibrary();
-		loader.loadManifest(lib.properties.manifest);
-	}
-
-	function handleFileLoad(evt, comp) {
-		var images = comp.getImages();
-		if (evt && (evt.item.type == "image")) {
-			images[evt.item.id] = evt.result;
-		}
-	}
-
-	function handleComplete(evt, comp) {
-		//This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-		var lib = comp.getLibrary();
-		var ss = comp.getSpriteSheet();
-		var queue = evt.target;
-		var ssMetadata = lib.ssMetadata;
-		for (i = 0; i < ssMetadata.length; i++) {
-			ss[ssMetadata[i].name] = new createjs.SpriteSheet({
-				"images": [queue.getResult(ssMetadata[i].name)],
-				"frames": ssMetadata[i].frames
-			})
-		}
-		exportRoot = new lib.animStart();
-		stage = new lib.Stage(canvas);
-		stage.enableMouseOver();
-		//Registers the "tick" event listener.
-		fnStartAnimation = function() {
-			stage.addChild(exportRoot);
-			createjs.Ticker.framerate = lib.properties.fps;
-			createjs.Ticker.addEventListener("tick", stage);
-		}
-		//Code to support hidpi screens and responsive scaling.
-		AdobeAn.makeResponsive(true, 'both', true, 1, [canvas, anim_container, dom_overlay_container]);
-		AdobeAn.compositionLoaded(lib.properties.id);
-		fnStartAnimation();
-	}
-</script>
-<!-- <a href="?section=game">Commencer le jeu</a> -->
